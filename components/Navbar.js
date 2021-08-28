7c406bc89fc4c754611b9db37b1ff9ededc9e387
tamdin <tgtamdin3@gmail.com>
2021-08-28 16:25:16 +0530

    fixed: hover effect on menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,12 +33,17 @@ export const Navbar = () => {
         console.log("dfkjvndfkj")
         const sidebar = document.querySelector(".sidebar")
         sidebar.classList.toggle("-translate-x-full")
+    };
 
+    const handleLargeMenu = () => {
+        console.log("dfkjvndfkj")
+        const largeMenu = document.querySelector(".large-menu")
+        largeMenu.classList.toggle("hidden")
     };
 
     return (
         <div>
-            <nav className="navbar relative w-full md:grid md:grid-cols-2 pt-6 md:pt-8 px-4 md:px-16 space-y-4 bg-white md:hover:border-b-2 border-gray-200">
+            <nav className="navbar fixed w-full pt-4 px-4 md:flex md:flex-col md:px-16 bg-white md:hover:border-b-2 border-gray-200">
                 <div className="md:hidden flex justify-between items-center align-middle">
                     <img onClick={handleMenu} className="md:hidden cursor-pointer" src="image/menu.png" alt="menu" />
                     <h1 className="text-2xl text-center font-bold cursor-pointer">bio:cule</h1>
@@ -47,60 +52,66 @@ export const Navbar = () => {
                         <img className="cursor-pointer" src="image/bag.png" alt="bag" />
                     </div>
                 </div>
-                <div className="md:block hidden">
-                    <h1 className="font-bold cursor-pointer text-4xl text-left">bio:cule</h1>
-                </div>
-                <div className="hidden md:flex justify-end space-x-10 align-middle items-center">
-                    <img className="cursor-pointer" src="image/insta.png" alt="instagram" />
-                    <img className="cursor-pointer" src="image/facebook.png" alt="facebook" />
-                    <img className="cursor-pointer" src="image/yt.png" alt="yt" />
-                    <img className="cursor-pointer" src="image/pin.png" alt="pin" />
-                    <img className="cursor-pointer" src="image/tw.png" alt="tw" />
+                <div className="flex justify-between">
+                    <div className="hidden md:flex">
+                        <h1 className="font-bold cursor-pointer text-4xl text-left">bio:cule</h1>
+                    </div>
+                    <div className="hidden md:flex justify-end space-x-10 align-middle items-center">
+                        <img className="cursor-pointer" src="image/insta.png" alt="instagram" />
+                        <img className="cursor-pointer" src="image/facebook.png" alt="facebook" />
+                        <img className="cursor-pointer" src="image/yt.png" alt="yt" />
+                        <img className="cursor-pointer" src="image/pin.png" alt="pin" />
+                        <img className="cursor-pointer" src="image/tw.png" alt="tw" />
+                    </div>
                 </div>
 
-                <ul className="hidden md:flex flex-row space-x-14">
-                    <li className="menu hover:border-b-2 border-black pb-4 cursor-pointer">
-                        <a href="#" className="text-gray-600 text-xl" >skin:care</a>
-                    </li>
-                    <li className="menu hover:border-b-2 border-black pb-4 cursor-pointer">
-                        <a href="#" className="text-gray-600 text-xl" >bio:edge</a>
-                    </li>
-                    <li className="menu hover:border-b-2 border-black pb-4 cursor-pointer">
-                        <a href="#" className="text-gray-600 text-xl" >b:logs</a>
-                    </li>
-                </ul>
-                <div className="hidden md:flex justify-end space-x-10 align-middle items-center">
-                    <img className="cursor-pointer" src="image/search.png" alt="search" />
-                    <img className="cursor-pointer" src="image/like.png" alt="like" />
-                    <img className="cursor-pointer" src="image/person.png" alt="person" />
-                    <img className="cursor-pointer" src="image/bag.png" alt="bag" />
+                <div className="flex justify-between items-center align-middle">
+                    <ul className="hidden md:flex space-x-14">
+                        <li onMouseEnter={handleLargeMenu} onMouseLeave={handleLargeMenu} className="menu relative hover:border-b-2 border-black py-4 cursor-pointer">
+                            <a href="#" className="text-gray-600 text-xl" >skin:care</a>
+                        </li>
+                        <li onMouseEnter={handleLargeMenu} onMouseLeave={handleLargeMenu} className="menu hover:border-b-2 border-black py-4 cursor-pointer">
+                            <a href="#" className="text-gray-600 text-xl" >bio:edge</a>
+                        </li>
+                        <li onMouseEnter={handleLargeMenu} onMouseLeave={handleLargeMenu} className="menu hover:border-b-2 border-black py-4 cursor-pointer">
+                            <a href="#" className="text-gray-600 text-xl" >b:logs</a>
+                        </li>
+                    </ul>
+                    <div className="hidden md:flex justify-end space-x-10">
+                        <img className="cursor-pointer" src="image/search.png" alt="search" />
+                        <img className="cursor-pointer" src="image/like.png" alt="like" />
+                        <img className="cursor-pointer" src="image/person.png" alt="person" />
+                        <img className="cursor-pointer" src="image/bag.png" alt="bag" />
+                    </div>
                 </div>
                 {/* // here goes all the menu for large screen */}
-                <div className="hidden md:grid grid-cols-3">
-                    <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
-                        {skinType.map((element) =>
-                            <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
-                        )}
-                    </ul>
-                    <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
-                        {skinGoal.map((element) =>
-                            <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
-                        )}
-                    </ul>   <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
-                        {productType.map((element) =>
-                            <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
-                        )}
-                        <div className="flex justify-between w-max space-x-2 align-middle items-center border-b-2 border-black">
-                            <a href="#" className="text-sm font-bold">
-                                All Products
-                            </a>
-                            <img src="image/arrow.png" />
-                        </div>
+                <div className="large-menu hidden">
+                    <div className="md:flex justify-between">
+                        <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
+                            {skinType.map((element) =>
+                                <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
+                            )}
+                        </ul>
+                        <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
+                            {skinGoal.map((element) =>
+                                <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
+                            )}
+                        </ul>   <ul className="text-lg font-bold py-3 cursor-pointer">Skin Type
+                            {productType.map((element) =>
+                                <li className="font-normal text-sm py-3 cursor-pointer">{element}</li>
+                            )}
+                            <div className="flex justify-between w-max pt-4 space-x-2 align-middle items-center border-b-2 border-black">
+                                <a href="#" className="text-sm font-bold">
+                                    All Products
+                                </a>
+                                <img src="image/arrow.png" />
+                            </div>
 
-                    </ul>
-                </div>
-                <div className="hidden md:flex justify-end">
-                    <img src="image/Frame.png" />
+                        </ul>
+                        <div className="">
+                            <img src="image/Frame.png" />
+                        </div>
+                    </div>
                 </div>
 
             </nav>
